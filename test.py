ed0b2cebf486d56f93407c02489db8941ba8e69c
Saideep@Rasengan <saitiku@gmail.com>
2021-12-17 21:49:50 -0700

    implemented tests

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 """
-Functions used for testing this module
+Functions used for testing and evaluation of this module
 """
 
 from helper import print_results, gen_test_numbers
 from tqdm.auto import tqdm
 from find_triplets import better_find, naive_find
 from typing import Callable
+from time import time
+from matplotlib import pyplot as plt
 
 def dummy_print_test():
     """Perform a dummy print test
@@ -20,13 +22,14 @@ def dummy_print_test():
 
 def test_triplet_finders():
     """see if naive_find and better_find work the same way
+    Assumes one of the two implementations are functionally correct
     """
 
     # iterate over possible length of list passed to our functions
-    for len_nums in [10, 100, 1_000, 10_000]:
+    for len_nums in [10, 100, 1_000]:
         print(f"list size:", len_nums)
-        # test on same number 10 times
-        for _ in tqdm(range(10)):
+        # test on same number 5 times
+        for _ in tqdm(range(5)):
             # gen some list of numbers
             numbers = gen_test_numbers(len_nums)
 
@@ -47,33 +50,63 @@ def test_triplet_finders():
                 else:
                     print(better_set - naive_set)
 
-                exit()
+                return
 
 
 def timed_test(finder: Callable):
     """return the run time of a function under different conditions
+
+    Parameters
+    ----------
+    finder : Callable
+        Takes input a list of numbers and returns set of triplets
     """
 
+    final_times = []
+
     # iterate over possible length of list passed to our functions
-    for len_nums in [10, 100, 1_000, 10_000]:
+    for len_nums in [10, 100, 1_000]:
         print(f"list size:", len_nums)
-        # test on same number 10 times
-        for _ in tqdm(range(10)):
+        times = []
+
+        # test on same number 5 times
+        for _ in tqdm(range(5)):
             # gen some list of numbers
             numbers = gen_test_numbers(len_nums)
 
-            triplet_set = finder(numbers)
+            # curr time
+            t = time()
+            _ = finder(numbers)  # execute, no need to save
+            # save elasped time
+            times.append(time() - t)
 
+        # take avg and save
+        final_times.append(sum(times) / 5)
+
+    return final_times
 
 
 if __name__ == "__main__":
     print("1. Test printing function")
     dummy_print_test()
     print("## 1. END ##\n")
+
     print("2. Test functionality.")
-    # Assumes one of the two implementations are functionally correct
     test_triplet_finders()
     print("## 2. END ##\n")
-    print("3. Make Time Plot")
-    print("## 3. END ##\n")
 
+    print("3. Make Time Plot. See times.png file.")
+    naive_times = timed_test(naive_find)
+    better_times = timed_test(better_find)
+    print("naive: ", naive_times)
+    print("better:", better_times)
+    # plot results
+    plt.plot(naive_times, label="Naive O(n^3)")
+    plt.plot(better_times, label="Better O(n^2)")
+    plt.legend()
+    plt.xlabel("Iters")
+    plt.ylabel("Time (seconds)")
+    plt.title("Execution Times")
+    plt.savefig("times.png")
+    # TODO: make the plot; creates another dependency :/
+    print("## 3. END ##\n")
